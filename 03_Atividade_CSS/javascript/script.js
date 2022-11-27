eabbcbd46a29e73f73da23034623d8c2531970a4
italoCesarSilvaOliveira <italo.cs.oliveira@gmail.com>
2022-11-27 19:11:02 -0300

    :sparkles: Página Principal Finalizada | Responsividade Pronta :sparkles:

--- 03_Atividade_CSS/javascript/script.js
+++ 03_Atividade_CSS/javascript/script.js
@@ -1,8 +1,10 @@
 var hamburguer = document.querySelector(".hamb");
 var navlist = document.querySelector(".nav-list");
 var links = document.querySelector(".nav-list li");
+var nav = document.querySelector("nav");
 
 hamburguer.addEventListener("click", function() {
   this.classList.toggle("click");
   navlist.classList.toggle("open");
+  nav.classList.toggle("visible");
 })
